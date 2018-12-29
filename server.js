80f70d1e6376698d1a90c7e42e06c6ab9a5b4f2b
Song <yousonghou@yahoo.com>
2018-12-29 17:18:48 +0000

    add a middleware to solve a issue with CORS errors

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    
+    next();
+});
+
 app.use('/users', userRoutes);
 app.use('/foods', foodRoutes);
 
