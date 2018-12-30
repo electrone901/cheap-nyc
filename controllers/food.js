1163ad415a9c917ea5a9c3adcff8723603f791bc
Song <yousonghou@yahoo.com>
2018-12-30 19:47:29 +0000

    add post food route and controller

--- controllers/food.js
+++ controllers/food.js
@@ -1,3 +1,30 @@
+const Food = require("../models/Food");
+
 exports.test = (req, res, next) => {
     res.json({msg: "Foods route works"});
+};
+
+exports.createFood = (req, res, next) => {
+    const name = req.body.name;
+    const price = req.body.price;
+    const location = req.body.location;
+    const description = req.body.description;
+    const userId = req.user.id;
+    
+    const food = new Food({
+        name: name,
+        price: price,
+        location: location,
+        description: description,
+        userId: userId
+    });
+    
+    food.save()
+        .then(result => {
+            res.status(201).json({
+                msg: "Success on creating a food post",
+                food: result
+            });
+        })
+        .catch(err => console.log(err));
 };
--- routes/food.js
+++ routes/food.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const passport = require('passport');
 
 const foodController = require("../controllers/food");
 
 const router = express.Router();
 
 router.get('/test', foodController.test);
+
+router.post('/', passport.authenticate('jwt', {session: false}), foodController.createFood);
 
 module.exports = router;
